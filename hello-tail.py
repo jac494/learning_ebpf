75efef20b8a75086e38dbda8714c65e551514f1e
Drew Conner <drew@blinkingboxes.net>
2023-07-12 23:10:15 -0500

    add hello-tail as example of tail calls

---
+++ hello-tail.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+from bcc import BPF, ct
+
+program = r"""BPF_PROG_ARRAY(syscall, 300);
+
+int hello(struct bpf_raw_tracepoint_args *ctx) {
+    int opcode = ctx->args[1];
+    syscall.call(ctx, opcode);
+    bpf_trace_printk("Another syscall: %d", opcode);
+    return 0;
+}
+
+int hello_execve(void *ctx) {
+    bpf_trace_printk("Executing a program");
+    return 0;
+}
+
+int hello_timer(struct bpf_raw_tracepoint_args *ctx) {
+    if (ctx->args[1] == 222) {
+    bpf_trace_printk("Creating a timer");
+    } else if (ctx->args[1] == 226) {
+        bpf_trace_printk("Deleting a timer");
+    } else {
+        bpf_trace_printk("Some other timer operation");
+    }
+    return 0;
+}
+
+int ignore_opcode(void *ctx) {
+    return 0;
+}
+"""
+
+b = BPF(text=program)
+b.attach_raw_tracepoint(tp="sys_enter", fn_name="hello")
+
+ignore_fn = b.load_func("ignore_opcode", BPF.RAW_TRACEPOINT)
+exec_fn = b.load_func("hello_execve", BPF.RAW_TRACEPOINT)
+timer_fn = b.load_func("hello_timer", BPF.RAW_TRACEPOINT)
+
+prog_array = b.get_table("syscall")
+prog_array[ct.c_int(59)] = ct.c_int(exec_fn.fd)
+prog_array[ct.c_int(222)] = ct.c_int(timer_fn.fd)
+prog_array[ct.c_int(223)] = ct.c_int(timer_fn.fd)
+prog_array[ct.c_int(224)] = ct.c_int(timer_fn.fd)
+prog_array[ct.c_int(225)] = ct.c_int(timer_fn.fd)
+prog_array[ct.c_int(226)] = ct.c_int(timer_fn.fd)
+
+# Ignore some syscalls that come up a lot
+for ignore_call_int in [0, 1, 14, 21, 22, 25, 28, 47, 202, 232, 270]:
+    prog_array[ct.c_int(ignore_call_int)] = ct.c_int(ignore_fn.fd)
+
+b.trace_print()
