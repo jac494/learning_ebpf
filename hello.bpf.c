a88e327f68f3bd1134e90ec60629726ec53b08bc
Drew Conner <drew@blinkingboxes.net>
2023-07-15 15:46:26 -0500

    add a simple intro bpf program in C

---
+++ hello.bpf.c
@@ -0,0 +1,14 @@
+#include <linux/types.h>
+#include <bpf/bpf_helpers.h>
+#include <linux/bpf.h>
+
+int counter = 0;
+
+SEC("xdp");
+int hello(void *ctx) {
+  bpf_printk("Hello world %d", counter);
+  counter++;
+  return XDP_PASS;
+}
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
